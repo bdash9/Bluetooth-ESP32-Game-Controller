9d47b015e217a890c6519b884ac6f633c6aa4c11
bdash9 <5065324+bdash9@users.noreply.github.com>
2023-10-21 08:38:07 -0400

    Update ESP32-multiple_buttons_BLE.ino

--- ESP32-multiple_buttons_BLE.ino
+++ ESP32-multiple_buttons_BLE.ino
@@ -1,4 +1,4 @@
-The code is based on the BleGamepad.h library https://github.com/lemmingDev/ESP32-BLE-Gamepad and the code example in Arduino IDE (File > Example > ESP32-BLE-Gamepad > SingleButton)
+// The code is based on the BleGamepad.h library https://github.com/lemmingDev/ESP32-BLE-Gamepad and the code example in Arduino IDE (File > Example > ESP32-BLE-Gamepad > SingleButton)
 #include <Arduino.h>
 #include <BleGamepad.h>
 
